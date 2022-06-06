48f12c3dd688f38cb9087a1682b3bb5c90aa62b7
Brian Shin <bshin132@gmail.com>
2022-06-05 18:04:38 -0700

    create other inputs

--- src/stories/Input.stories.js
+++ src/stories/Input.stories.js
@@ -6,5 +6,11 @@ export default {
   component: Input,
 };
 
-export const SearchFood = () => <Input label="Search for food..."/>;
-export const SearchRestaurant = () => <Input label="Search for restaurant..."/>;
+export const SearchFood = () => <Input label="Search for food..." />;
+export const SearchRestaurant = () => <Input label="Search for restaurant..." />;
+export const Email = () => <Input label="Email"borderRadius="0" />;
+export const Password = () => <Input label="Password" borderRadius="0" />;
+export const FirstName = () => <Input label="First Name" borderRadius="0" />;
+export const LastName = () => <Input label="Last Name" borderRadius="0" />;
+export const PhoneNumber = () => <Input label="Phone Number" borderRadius="0" />;
+export const Location = () => <Input label="Location" borderRadius="0" />;
