3b94adfa8a9db63c91c1f224d0339800f63fc893
Brian Shin <bshin132@gmail.com>
2022-06-05 17:00:06 -0700

    create apply, save changes, order now, signup buttons

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -8,3 +8,7 @@ export default {
 
 export const LoginButton = () => <Button label="LOGIN" />;
 export const LogoutButton = () => <Button label="LOGOUT" backgroundColor="#F3A85F" />;
+export const Signup = () => <Button label="SIGNUP" backgroundColor="#F3A85F" />;
+export const Apply = () => <Button label="APPLY" backgroundColor="#F3A85F" />;
+export const OrderNow = () => <Button label="ORDER NOW" backgroundColor="#F3A85F" />;
+export const SaveChanges = () => <Button label="SAVE CHANGES" backgroundColor="#F3A85F" />;
